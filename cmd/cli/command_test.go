bdc6641b6df4e6e956f880c3f7500324e3d1f928
Jon Stevens <jon@hotaisle.xyz>
2026-01-04 14:20:31 -0800

    fix: null check and throw an error if command can't be found

--- cmd/cli/command_test.go
+++ cmd/cli/command_test.go
@@ -1,20 +1,26 @@
 package cli
 
-import "github.com/urfave/cli/v3"
+import (
+	"errors"
+
+	"github.com/urfave/cli/v3"
+)
 
 // getCommand looks up a command by path and sets flags for testing
 func getCommand(app *App, def commandDef, path string, flags map[string]string) (*cli.Command, error) {
 	cmdDef := def.findCommand(path)
 	if cmdDef == nil {
-		return nil, nil
+		return nil, errors.New("command not found: " + path)
 	}
 
 	cmd := buildCommand(app, *cmdDef)
 
 	// Set flags if provided
-	for name, value := range flags {
-		if err := cmd.Set(name, value); err != nil {
-			return nil, err
+	if flags != nil {
+		for name, value := range flags {
+			if err := cmd.Set(name, value); err != nil {
+				return nil, err
+			}
 		}
 	}
 
